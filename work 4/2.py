cc7ed87ddb3a477423b91b530717694bfd68e0da
AlexWeb2018 <38156259+AlexWeb2018@users.noreply.github.com>
2019-10-16 11:39:01 +0300

    Add files via upload

--- work 4/2.py
+++ work 4/2.py
@@ -1,2 +1,8 @@
 pi = 3.14
 a = int(input("Введите градусную меру 0 <= A < 360: "))
+
+if(0 <= a and 360 > a):
+	rad = pi * a / 180
+	print(rad)
+else:
+	print("Ниправильнаааа")
---
+++ work 4/3.py
@@ -0,0 +1,4 @@
+x = int(input("ведите x: "))
+a = int(input("ведите a: "))
+res = a/x
+print("стоимость" , res)
