27e26c358ea1809d3fe89d5565998766c0941c17
AlexWeb2018 <38156259+AlexWeb2018@users.noreply.github.com>
2019-10-29 17:51:16 +0300

    Add files via upload

---
+++ 6.4.py
@@ -0,0 +1,9 @@
+import random
+
+N = int(input("Введите число: "))
+d1 = int(N / 10)
+d0 = N % 10
+N_new = d0 * 10 + d1
+print("Десятки: ", d1)
+print("Единицы: ", d0)
+print("Новое число: ", N_new)
